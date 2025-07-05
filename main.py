f934d3cdcc44cee28eff4f399427172301220954
Mahmedorabi <mahmedorabi297@gmail.com>
2025-07-05 04:04:50 +0300

    change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.binance_client import stream_data
 
 app = FastAPI()
 
-@app.get("/health")
+@app.get("/")
 async def health():
     return {"status": "ok"}
 
@@ -13,7 +13,5 @@ async def run_streamer():
     await stream_data()
 
 if __name__ == "__main__":
-    # start data streamer in the background
     asyncio.create_task(run_streamer())
-    # run HTTP server
     uvicorn.run(app, host="0.0.0.0", port=8000)
