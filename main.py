021f181d6b6728aace6b3820bbfc67af4e0b114b
Mahmedorabi <mahmedorabi297@gmail.com>
2025-07-05 13:00:47 +0300

    change logic

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from fastapi import FastAPI
-import uvicorn
 import asyncio
 from src.binance_client import stream_data
 
-app = FastAPI()
-
-@app.get("/")
-async def health():
-    return {"status": "ok"}
-
-async def run_streamer():
-    await stream_data()
 
 if __name__ == "__main__":
-    asyncio.create_task(run_streamer())
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    asyncio.run(stream_data())
